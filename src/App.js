e50fe94d4dcb3c889836c9f3541e8a2a38dc2815
Chirag Soni <chiragsoni81245@gmail.com>
2022-01-22 02:17:39 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, This Site Deployed with JAM Stack</h1>
-      <h2>Did it worked?</h2>
+      <h2>It worked!</h2>
     </div>
   );
 }
